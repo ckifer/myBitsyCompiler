f0acd215864a4604004906dc4f2093bea59e6f12
Coltin Kifer <ck108860@yahoo.com>
2018-02-13 20:08:21 -0500

    Initial Commit

---
+++ myBitsyCompiler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  myBitsyCompiler
+//
+//  Created by Coltin Kifer on 2/13/18.
+//  Copyright © 2018 Coltin Kifer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
